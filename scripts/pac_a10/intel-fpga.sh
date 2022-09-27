531cba1dcf100d5723a874dd4a18d645a31c05f8
Elias Koromilas <elias.koromilas@gmail.com>
2022-09-27 19:56:02 +0300

    Update host Intel PAC S10 DC RTE for OpenCL

--- scripts/pac_a10/intel-fpga.sh
+++ scripts/pac_a10/intel-fpga.sh
@@ -25,7 +25,7 @@ if [ ${ID} = centos ]; then
 
 	# Download Intel FPGA packages
 	wget -O- "https://downloads.intel.com/akdlm/software/ias/1.2.1/a10_gx_pac_ias_1_2_1_pv_rte.tar.gz" | tar xz --strip-components 1
-	wget -O aocl-pro-rte.run "https://downloads.intel.com/akdlm/software/acdsinst/20.4/72/ib_installers/aocl-pro-rte-20.4.0.72-linux.run" && chmod +x aocl-pro-rte.run
+	wget -O aocl-pro-rte-20.4.run "https://downloads.intel.com/akdlm/software/acdsinst/20.4/72/ib_installers/aocl-pro-rte-20.4.0.72-linux.run" && chmod +x aocl-pro-rte-20.4.run
 
 	# Download InAccel runtime
 	wget -O inaccel-fpga.rpm "https://dl.cloudsmith.io/public/inaccel/stable/rpm/any-distro/any-version/x86_64/inaccel-fpga-${INACCEL_FPGA}-1.x86_64.rpm"
@@ -38,7 +38,7 @@ if [ ${ID} = centos ]; then
 	# Install Extra Packages for Enterprise Linux (EPEL)
 	yum install -y epel-release
 
-	# Install Kernel Headers (required by Xilinx FPGA packages)
+	# Install Kernel Headers (required by Intel FPGA packages)
 	if ! yum install -y kernel-devel-$(uname -r) kernel-headers-$(uname -r); then
 		# Kernel Headers not found, retry using the CentOS Vault
 		yum install -y ${VAULT}/kernel-devel-$(uname -r).rpm ${VAULT}/kernel-headers-$(uname -r).rpm
@@ -47,7 +47,7 @@ if [ ${ID} = centos ]; then
 	# Install Intel FPGA packages
 	sed -e "s|more |cat |g" -e "s|yum install|yum install -y|g" -i setup.sh
 	yes | ${PWD}/setup.sh --installdir /opt --yes
-	${PWD}/aocl-pro-rte.run --accept_eula 1 --installdir /opt/opencl_rte/20.4 --mode unattended
+	${PWD}/aocl-pro-rte-20.4.run --accept_eula 1 --installdir /opt/opencl_rte/20.4 --mode unattended
 	cat << EOF > /opt/intelrtestack/init_devices.sh
 source /opt/intelrtestack/init_env.sh
 aocl list-devices | grep -A 1 --no-group-separator "Device Name" | grep -v "Device Name" | xargs -i aocl program {} /opt/intelrtestack/a10_gx_pac_ias_1_2_1_pv/opencl/hello_world.aocx
@@ -70,7 +70,7 @@ elif [ ${ID} = ubuntu ]; then
 
 	# Download Intel FPGA packages
 	wget -O- "https://downloads.intel.com/akdlm/software/ias/1.2.1/a10_gx_pac_ias_1_2_1_pv_rte.tar.gz" | tar xz --strip-components 1
-	wget -O aocl-pro-rte.run "https://downloads.intel.com/akdlm/software/acdsinst/20.4/72/ib_installers/aocl-pro-rte-20.4.0.72-linux.run" && chmod +x aocl-pro-rte.run
+	wget -O aocl-pro-rte-20.4.run "https://downloads.intel.com/akdlm/software/acdsinst/20.4/72/ib_installers/aocl-pro-rte-20.4.0.72-linux.run" && chmod +x aocl-pro-rte-20.4.run
 
 	# Download InAccel runtime
 	wget -O inaccel-fpga.deb "https://dl.cloudsmith.io/public/inaccel/stable/deb/any-distro/pool/any-version/main/i/in/inaccel-fpga_${INACCEL_FPGA}/inaccel-fpga_${INACCEL_FPGA}_amd64.deb"
@@ -78,7 +78,7 @@ elif [ ${ID} = ubuntu ]; then
 	# Install Intel FPGA packages
 	sed -e "s|more |cat |g" -e "s|apt-get install|apt-get install -y|g" -i setup.sh
 	yes | ${PWD}/setup.sh --installdir /opt --yes
-	${PWD}/aocl-pro-rte.run --accept_eula 1 --installdir /opt/opencl_rte/20.4 --mode unattended
+	${PWD}/aocl-pro-rte-20.4.run --accept_eula 1 --installdir /opt/opencl_rte/20.4 --mode unattended
 	cat << EOF > /opt/intelrtestack/init_devices.sh
 source /opt/intelrtestack/init_env.sh
 aocl list-devices | grep -A 1 --no-group-separator "Device Name" | grep -v "Device Name" | xargs -i aocl program {} /opt/intelrtestack/a10_gx_pac_ias_1_2_1_pv/opencl/hello_world.aocx
--- scripts/pac_s10_dc/intel-fpga.sh
+++ scripts/pac_s10_dc/intel-fpga.sh
@@ -25,7 +25,8 @@ if [ ${ID} = centos ]; then
 
 	# Download Intel FPGA packages
 	wget -O- "https://downloads.intel.com/akdlm/software/ias/2.0.1/d5005_pac_ias_2_0_1_pv_rte_installer.tar.gz" | tar xz --strip-components 1
-	wget -O aocl-pro-rte.run "https://downloads.intel.com/akdlm/software/acdsinst/20.4/72/ib_installers/aocl-pro-rte-20.4.0.72-linux.run" && chmod +x aocl-pro-rte.run
+	wget -O aocl-pro-rte.run "https://downloads.intel.com/akdlm/software/acdsinst/19.4/64/ib_installers/aocl-pro-rte-19.4.0.64-linux.run" && chmod +x aocl-pro-rte.run
+	wget -O aocl-pro-rte-20.4.run "https://downloads.intel.com/akdlm/software/acdsinst/20.4/72/ib_installers/aocl-pro-rte-20.4.0.72-linux.run" && chmod +x aocl-pro-rte-20.4.run
 
 	# Download InAccel runtime
 	wget -O inaccel-fpga.rpm "https://dl.cloudsmith.io/public/inaccel/stable/rpm/any-distro/any-version/x86_64/inaccel-fpga-${INACCEL_FPGA}-1.x86_64.rpm"
@@ -38,7 +39,7 @@ if [ ${ID} = centos ]; then
 	# Install Extra Packages for Enterprise Linux (EPEL) & Python 3
 	yum install -y epel-release python3
 
-	# Install Kernel Headers (required by Xilinx FPGA packages)
+	# Install Kernel Headers (required by Intel FPGA packages)
 	if ! yum install -y kernel-devel-$(uname -r) kernel-headers-$(uname -r); then
 		# Kernel Headers not found, retry using the CentOS Vault
 		yum install -y ${VAULT}/kernel-devel-$(uname -r).rpm ${VAULT}/kernel-headers-$(uname -r).rpm
@@ -47,7 +48,8 @@ if [ ${ID} = centos ]; then
 	# Install Intel FPGA packages
 	sed -e "s|more |cat |g" -e "s|yum install|yum install -y|g" -i setup.sh
 	yes | ${PWD}/setup.sh --installdir /opt --yes
-	${PWD}/aocl-pro-rte.run --accept_eula 1 --installdir /opt/opencl_rte/20.4 --mode unattended
+	${PWD}/aocl-pro-rte.run --accept_eula 1 --installdir /opt/opencl_rte --mode unattended
+	${PWD}/aocl-pro-rte-20.4.run --accept_eula 1 --installdir /opt/opencl_rte/20.4 --mode unattended
 	cat << EOF > /opt/intelrtestack/init_devices.sh
 source /opt/intelrtestack/init_env.sh
 aocl list-devices | grep -A 1 --no-group-separator "Device Name" | grep -v "Device Name" | xargs -i aocl program {} /opt/intelrtestack/d5005_ias_2_0_1_b237/opencl/hello_world.aocx
